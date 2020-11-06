82511b318e9e04932197dc1180c4f525654eb505
xiaoludanzai <1375875103@qq.com>
2020-11-06 20:58:10 +0800

    修正加锁部分

--- web/src/main/java/com/hackerda/platform/service/EmptyRoomService.java
+++ web/src/main/java/com/hackerda/platform/service/EmptyRoomService.java
@@ -72,7 +72,7 @@ public class EmptyRoomService {
 	  try { 
 			Map<String, EmptyRoom> classRoomMap = new HashMap<>();
 			List<Integer> orderList = Lists.newArrayList(1, 3, 5, 7, 9);
-			//加锁部分。
+			//加锁部分
 			if (stringRedisTemplate.keys("empty_Room_data::" + week + teaNum + "*").size() == 0) {
 				if (!lockMap.containsKey(week + teaNum)) {
 					synchronized (this) {
